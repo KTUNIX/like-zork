ed3e854f1d1b48a9ed13a81dfe12c97b5a2a39f2
onakin_mania <124423754+onakinmania@users.noreply.github.com>
2023-04-08 15:49:20 +0900

    Add files via upload

---
+++ tntn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while 
+	echo -n \
+		 '*1) 逃げる
+	         *2) 歩く
+		 *3) 拾う
+		 *4) 確認
+		 *コマンド？ ' 1>&2 
+	read cmd 
+do
+	case $cmd in
+		1) 
+		  echo '*無理でした';;
+		2)
+		  echo '*転んで死にました'
+                  echo '*終了します'
+                  sleep 1  
+		  break;;
+        esac
+        echo
+done
